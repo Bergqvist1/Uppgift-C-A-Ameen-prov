048767a0d37eb14208afb1aa35381559150c4917
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-02-17 11:12:59 +0100

    Add project files.

---
+++ Uppgift C-A, Ameen prov/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Uppgift_C_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj en av omvandlings metoderna:");
+            Console.WriteLine("1: Omvandla Celsius till Fahrenheit");
+            Console.WriteLine("2: Omvandla Fahrenheit till Celsius");
+            
+            
+
+            int val = int.Parse(Console.ReadLine());
+
+            switch(val)
+            {
+                case 1:
+                    Console.WriteLine("Skriv in grader Celsius du vill omvandla till grader Fahrenheit:");
+                    double C = double.Parse(Console.ReadLine());
+                    double F = ((32 + C) * 1.8);
+                    Console.WriteLine($"{C} grader C = " + CelsiusTillFahrenheit(C,F) + " grader i Fahrenheit");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Skriv in grader Fahrenheit du vill omvandla till grader Celsius:");
+                    double Fahrenheit = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{Fahrenheit} grader F = " + FahrenheitTillCelsius(C,F) + " grader i Fahrenheit");
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+      
+        static double CelsiusTillFahrenheit(double C, double F)
+        {
+            
+            double Fahrenheit = ((32+C)*1.8);
+            return Fahrenheit;
+        }
+
+        static double FahrenheitTillCelsius(double C, double F)
+        {
+            double Celsius = ((F - 32) / 1.8);
+            return Celsius;
+        }
+    }
+}
